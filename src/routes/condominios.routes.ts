ec6da1f12498869bd1297f1eb59d1ee872d3a253
PCBorges10 <pcosta.borgess@gmail.com>
2026-09-22 13:09:20 -0300

    Adiciona cadastro e busca por id de condominios

--- src/routes/condominios.routes.ts
+++ src/routes/condominios.routes.ts
@@ -1,10 +1,74 @@
 import { FastifyInstance } from 'fastify'
 import { db } from '../database/connection.js'
 
+interface CondominioBody {
+  nome: string
+  cnpj: string
+  endereco: string
+  numero: string
+  bairro: string
+  cidade: string
+  estado: string
+  cep: string
+  telefone: string
+  email: string
+}
+
+interface CondominioParams {
+  id: string
+}
+
 export async function condominiosRoutes(app: FastifyInstance) {
   app.get('/condominios', async () => {
     const [rows] = await db.query('SELECT * FROM condominios')
 
     return rows
+  })
+  
+app.get<{ Params: CondominioParams }>('/condominios/:id', async (request, reply) => {
+  const { id } = request.params
+
+  const [rows] = await db.query(
+    'SELECT * FROM condominios WHERE id = ?',
+    [id]
+  )
+
+  return rows
+})
+  app.post<{ Body: CondominioBody }>('/condominios', async (request, reply) => {
+    const {
+      nome,
+      cnpj,
+      endereco,
+      numero,
+      bairro,
+      cidade,
+      estado,
+      cep,
+      telefone,
+      email
+    } = request.body
+
+    await db.execute(
+      `INSERT INTO condominios
+  (nome, cnpj, endereco, numero, bairro, cidade, estado, cep, telefone, email)
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+      [
+        nome,
+        cnpj,
+        endereco,
+        numero,
+        bairro,
+        cidade,
+        estado,
+        cep,
+        telefone,
+        email
+      ]
+    )
+
+    return reply.status(201).send({
+      message: 'Condomínio cadastrado com sucesso!'
+    })
   })
 }
